b4953c4fe27ca64d7bd804736188d87d6e4d2f5a
Róbert Čerňanský <openhs@tightmail.com>
2016-07-04 16:31:12 +0200

    Do not wait for async. network operations to finish, ignore them instead.

--- src/ampache_browser/ampache_browser_plugin.cc
+++ src/ampache_browser/ampache_browser_plugin.cc
@@ -59,19 +59,29 @@ private:
 
     void onSettingsChanged();
 
-    void onFinished();
-
-    void networkRequest(const std::string& url, ApplicationQt::NetworkRequestCb& networkRequestCb);
-
     static Index<PlaylistAddItem> createPlaylistItems(const vector<string>& trackUrls);
     static int getVerbosity();
 };
 
 
 
+bool isInitialized = false;
+
+
+
 void onVfsAsyncFileGetContentsCb(const char* url, const Index<char>& buffer, void* userData) {
-    auto& callback = *reinterpret_cast<ApplicationQt::NetworkRequestCb*>(userData);
-    callback(url, buffer.begin(), buffer.len());
+    if (isInitialized) {
+        auto& callback = *reinterpret_cast<ApplicationQt::NetworkRequestCb*>(userData);
+        callback(url, buffer.begin(), buffer.len());
+    } else {
+        AUDDBG("Ignoring network request result for URL '%s'.\n", url);
+    }
+}
+
+
+
+void networkRequest(const string& url, ApplicationQt::NetworkRequestCb& networkRequestCb) {
+    vfs_async_file_get_contents(url.c_str(), onVfsAsyncFileGetContentsCb, &networkRequestCb);
 }
 
 
@@ -86,7 +96,7 @@ const char AmpacheBrowserPlugin::about[] =
 
 bool AmpacheBrowserPlugin::init() {
     myApplicationQt = unique_ptr<ApplicationQt>{new ApplicationQt{}};
-    myApplicationQt->setNetworkRequestFunction(bind(&AmpacheBrowserPlugin::networkRequest, this, _1, _2));
+    myApplicationQt->setNetworkRequestFunction(networkRequest);
 
     mySettings = &myApplicationQt->getSettings();
     mySettings->setBool(Settings::USE_DEMO_SERVER, aud_get_bool(SETTINGS_SECTION, Settings::USE_DEMO_SERVER.c_str()));
@@ -103,16 +113,16 @@ bool AmpacheBrowserPlugin::init() {
     myAmpacheBrowser->connectCreatePlaylist(bind(&AmpacheBrowserPlugin::onAmpacheBrowserCreatePlaylist, this, _1));
     myAmpacheBrowser->connectAddToPlaylist(bind(&AmpacheBrowserPlugin::onAmpacheBrowserAddToPlaylist, this, _1));
 
+    isInitialized = true;
+
     return true;
 }
 
 
 
 void AmpacheBrowserPlugin::cleanup() {
-    // if the body of onFinished method would be defined here (as anonymous function), destroying of myApplicationQt
-    // would invalidate the captured 'this' pointer which would cause segfault when accessing instance variables later
-    // on; therefore onFinished cannot be defined as anonymous
-    myApplicationQt->finishRequest(bind(&AmpacheBrowserPlugin::onFinished, this));
+    isInitialized = false;
+    myApplicationQt = nullptr;
 }
 
 
@@ -149,19 +159,6 @@ void AmpacheBrowserPlugin::onSettingsChanged() {
     aud_set_str(SETTINGS_SECTION, Settings::USER_NAME.c_str(), mySettings->getString(Settings::USER_NAME).c_str());
     aud_set_str(SETTINGS_SECTION, Settings::PASSWORD_HASH.c_str(),
         mySettings->getString(Settings::PASSWORD_HASH).c_str());
-}
-
-
-
-void AmpacheBrowserPlugin::onFinished() {
-    AUDINFO("Finishing.\n");
-    myApplicationQt = nullptr;
-}
-
-
-
-void AmpacheBrowserPlugin::networkRequest(const string& url, ApplicationQt::NetworkRequestCb& networkRequestCb) {
-    vfs_async_file_get_contents(url.c_str(), onVfsAsyncFileGetContentsCb, &networkRequestCb);
 }
 
 
